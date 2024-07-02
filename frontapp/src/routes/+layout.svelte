491a16fc3563c258c56f0eaeeb135662ac388d49
psm817 <5004jse@gmail.com>
2024-07-02 18:34:10 +0900

    me, login

--- frontapp/src/routes/+layout.svelte
+++ frontapp/src/routes/+layout.svelte
@@ -1,9 +1,41 @@
 <script>
-    import "../app.css";
+    import {onMount} from 'svelte';
+
+    let member = {};
+    let isLogin = false;
+
+    onMount(() => {
+        // 로그인한 회원 정보 불러오기
+        fetch('http://localhost:8090/api/v1/members/me', {
+            credentials: "include"
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // 회원 정보 불러오기 성공 시 데이터를 member에 담기
+                console.log(data.data?.item);
+                member = data.data?.item;
+                isLogin = true;
+            })
+            .catch(error => {
+                // 불러오기 실패 시 처리
+                console.error(error);
+            })
+    });
+
 </script>
 
-<div class="text-orange-600">레이아웃</div>
-<a href="/articles">게시물 목록</a>
-<a href="/test">테스트</a>
+<header>
+    <ul>
+        {#if isLogin}
+        <li><a href="/logout">로그아웃</a></li>
+        {:else}
+        <li><a href="/login">로그인</a></li>
+        {/if}
+    </ul>
+</header>
+
+<div>
+    <h2>id : {member.username}</h2>
+</div>
 
 <slot />
--- src/main/java/com/example/sksb/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/sksb/domain/member/controller/ApiV1MemberController.java
@@ -12,10 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/members")
@@ -57,6 +54,24 @@ public class ApiV1MemberController {
                                 authAndMakeTokensRs.getData().getMember()
                         )
                 )
+        );
+    }
+
+    @Getter
+    public static class MeResponseBody {
+        private MemberDto item;
+
+        public MeResponseBody(Member member) {
+            this.item = new MemberDto(member);
+        }
+    }
+
+    @GetMapping(value = "/me")
+    public RsData<MeResponseBody> getMe() {
+        return RsData.of(
+                "200",
+                "내 정보 가져오기 성공",
+                new MeResponseBody(rq.getMember())
         );
     }
 }
